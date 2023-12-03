bf6512202d58c5404a04f2ade10e7ecd5df59cad
Zeyad Zewail <ZeyadZewail@gmail.com>
2023-12-03 02:09:46 +0200

    real Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import { AuthGuard } from '@/Components/AuthGuard/AuthGuard';
-
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,9 +16,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <AuthGuard>{children}</AuthGuard>
-            </body>
+            <body className={inter.className}>{children}</body>
         </html>
     );
 }
---
+++ middleware.ts
@@ -0,0 +1,53 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { pb } from '@/Util/Pocketbase';
+
+export async function middleware(request: NextRequest) {
+    const response = NextResponse.next();
+    const request_cookie = request.cookies.get('pb_auth');
+    pb.authStore.clear();
+
+    if (request.nextUrl.pathname.startsWith('/_next')) {
+        return NextResponse.next();
+    }
+
+    if (request_cookie) {
+        try {
+            const jsonCookie = JSON.parse(request_cookie.value);
+            pb.authStore.save(jsonCookie.token, jsonCookie.model);
+        } catch (error) {
+            response.cookies.set(request_cookie.name, request_cookie.value);
+        }
+    }
+
+    try {
+        // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+        pb.authStore.isValid && (await pb.collection('users').authRefresh());
+    } catch (err) {
+        response.cookies.set(
+            'pb_auth',
+            pb.authStore.exportToCookie({ httpOnly: false })
+        );
+    }
+
+    if (request.nextUrl.pathname.startsWith('/auth')) {
+        return response;
+    }
+
+    if (
+        !pb.authStore.isValid &&
+        !request.nextUrl.pathname.startsWith('/auth')
+    ) {
+        return NextResponse.redirect(new URL('/auth', request.url));
+    }
+
+    if (pb.authStore.isValid && request.nextUrl.pathname.startsWith('/auth')) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
+    return response;
+}
+
+export const config = {
+    matcher: ['/:path*', '/auth/:path*'],
+};
