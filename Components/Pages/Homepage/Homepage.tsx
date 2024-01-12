2e7c63f05aaf0864dcf9cf5d20564c2883ba40c7
Zeyad Zewail <ZeyadZewail@gmail.com>
2024-01-12 23:44:35 +0200

    misc

--- Components/Pages/Homepage/Homepage.tsx
+++ Components/Pages/Homepage/Homepage.tsx
@@ -6,7 +6,6 @@ const Homepage = async ({ slug }: { slug: string }) => {
         <div className="flex flex-col p-16 gap-8">
             <h1 className="text-7xl self-center">Collections</h1>
             <LogOutButton />
-
             <CollectionsList />
         </div>
     );
